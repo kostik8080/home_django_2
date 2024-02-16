80f859dead11e62cb5022ee7a025a9c338b931d7
Konstantin Onishchenko <kostik80_80@mail.ru>
2024-02-16 17:52:40 +0300

    удаление миграци

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -6,7 +7,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     def __str__(self):
-        return 'Здесь будет модель Category'
+        return f'{self.name} ({self.description})'
 
     class Meta:
         pass
@@ -19,8 +20,11 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата изменения')
+    # manufactured_at = models.DateField(default=timezone.now)
+
+
     def __str__(self):
-        return 'Здесь будет модель Product'
+        return f'{self.name} {self.description} {self.photo} {self.category} {self.price} {self.created_at} {self.updated_at}'
 
     class Meta:
         pass
