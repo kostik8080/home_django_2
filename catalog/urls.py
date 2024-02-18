50d540bcf5cfa411508bcf864176cb8a12dd9fd6
Konstantin Onishchenko <kostik80_80@mail.ru>
2024-02-18 20:19:32 +0300

    Исправление ошибки на уникальность id

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,5 @@ from catalog.views import index, contact
 urlpatterns = [
     path('', index),
     path('contacts/', contact),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Product
+from django.template.loader import render_to_string
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    products = Product.objects.order_by('-id')[:5]
+    data_list = []
+    for product in products:
+        data_list.append(product.name)
+
+    return render(request, 'catalog/index.html', {'products': data_list})
 
 
 def contact(request):
@@ -12,3 +19,5 @@ def contact(request):
         message = request.POST.get('message')
         print(f"{name} ({phone}): {message}")
     return render(request, 'catalog/contact.html')
+
+
